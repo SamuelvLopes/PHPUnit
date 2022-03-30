9b4ffcd1693d244fdc4c6216c6accaad472b4ff1
Samuel Lopes <samuelvitorprofissional@gmail.com>
2022-03-30 16:22:17 -0300

    correção no autoload e inicio de testes

---
+++ testando com phpunit/src/OrderBundle/Test/Validators/NotEmptyValidatorTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace OrderBundle\Validators\Test;
+
+use OrderBundle\Validators\NotEmptyValidator;
+use PHPUnit\Framework\TestCase;
+
+
+class NotEmptyValidatorTest extends TestCase{
+
+    public function testShouldNotBeValidWhenValueIsEmpty(){
+
+        $emptyValue = "";
+        $notEmptyValidator = new NotEmptyValidator($emptyValue);
+        $isValid = $notEmptyValidator->isValid();
+        
+        $this->assertFalse( $isValid);
+
+    }
+
+}
