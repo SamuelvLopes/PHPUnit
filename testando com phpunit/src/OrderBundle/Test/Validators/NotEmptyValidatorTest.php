c8a981f748a2b32169367fc21b4e5a1a384b0bab
Samuel Lopes <samuelvitorprofissional@gmail.com>
2022-04-07 03:53:42 -0300

    adicionando novas possibilidades

--- testando com phpunit/src/OrderBundle/Test/Validators/NotEmptyValidatorTest.php
+++ testando com phpunit/src/OrderBundle/Test/Validators/NotEmptyValidatorTest.php
@@ -14,8 +14,29 @@ class NotEmptyValidatorTest extends TestCase{
         $notEmptyValidator = new NotEmptyValidator($emptyValue);
         $isValid = $notEmptyValidator->isValid();
         
-        $this->assertFalse( $isValid);
+        $this->assertFalse($isValid);
 
     }
+    public function testShouldBeValidWhenValueIsNotEmpty(){
 
+        $notemptyValue = "valor";
+        $notEmptyValidator = new NotEmptyValidator($notemptyValue);
+        $isValid = $notEmptyValidator->isValid();
+        
+        $this->assertTrue($isValid);
+
+    }
+    public function testDataProvides(){
+        $dataProvider=[
+            ''=>false,
+            'dd'=>true
+
+        ];
+        foreach($dataProvider as $value =>$expectedResult){
+        $Validator = new NotEmptyValidator($value);
+        $isValid = $Validator->isValid();
+        
+        $this->assertEquals($expectedResult,$isValid);
+        }
+    }
 }
